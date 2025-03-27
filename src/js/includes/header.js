844196d901fca981acd8e604ce9d9f4551caf789
로인 <151013952+bboroin@users.noreply.github.com>
2025-03-27 19:17:21 +0900

    feat: navigation__submenu 영역 애니메이션 효과 추가 #5

--- src/js/includes/header.js
+++ src/js/includes/header.js
@@ -46,7 +46,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
-    /*** 네비게이션창 -  navigation__btn ***/
     const buttons = document.querySelectorAll('.navigation__btn, .navigation__btn--active, .submenu-item__btn');
     const submenus = document.querySelectorAll('.navigation__submenu');
 
@@ -63,10 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
             // 같은 최상위 서브메뉴 내부의 다른 서브메뉴만 닫기
             document.querySelectorAll('.navigation__submenu').forEach(sub => {
                 if (sub !== submenu && sub !== parentMenu) { // 클릭한 서브메뉴와 부모는 제외
-                    sub.style.display = 'none';
-                    sub.style.height = '0px';
-
-                    // 아이콘 초기화
+                    sub.classList.remove('open'); // open 클래스 제거
                     const iconReset = sub.previousElementSibling?.querySelector('.icon-chevron-up, .icon-chevron-down');
                     if (iconReset) {
                         iconReset.classList.remove('icon-chevron-up');
@@ -78,31 +74,27 @@ document.addEventListener("DOMContentLoaded", () => {
             });
 
             // 이미 열린 서브메뉴를 다시 클릭했는지 확인
-            const isOpen = submenu.style.display === "block";
+            const isOpen = submenu.classList.contains('open');
 
             // 만약 클릭한 버튼이 이미 열린 상태라면 닫기만 수행
             if (isOpen) {
-                submenu.style.display = "none";
-                submenu.style.height = "0px"; // 서브메뉴 닫기
-
+                submenu.classList.remove('open'); // 서브메뉴 닫기
                 icon.classList.remove('icon-chevron-up');
                 icon.classList.add('icon-chevron-down');
                 const img = icon.querySelector('img');
                 img.src = '/src/assets/icons/header/icon-chevron-down.png'; // down 이미지로 변경
-
                 return; // 여기서 함수 종료 (새로 열지 않음)
             }
 
             // 클릭한 버튼의 서브메뉴를 열기
-            submenu.style.display = "block";
-            submenu.style.height = "auto"; // 서브메뉴 열기
-
+            submenu.classList.add('open');
             icon.classList.remove('icon-chevron-down');
             icon.classList.add('icon-chevron-up');
             const img = icon.querySelector('img');
             img.src = '/src/assets/icons/header/icon-chevron-up.png'; // up 이미지로 변경
         });
     });
+
 
 
     /*** 검색창 (mobile) ***/
