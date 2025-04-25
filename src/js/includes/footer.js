e86b51e533e3eec2a547608c34eba1c073edcfd8
로인 <151013952+bboroin@users.noreply.github.com>
2025-04-25 14:03:54 +0900

    refactor: footer__company-icon 에 font-family 적용 및 그에 따른 기능 수정 #33

--- src/js/includes/footer.js
+++ src/js/includes/footer.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('footer__company-btn').addEventListener('click', function () {
-        const icon = document.getElementById('footer__company-icon').querySelector('img');
+        const footerIcon = document.querySelector('.footer__company-icon');
         const companyInfo = document.querySelector('.footer__company-info');
 
         // footer__company-info의 클래스 토글 (열리고 닫히는 동작)
@@ -8,9 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // 아이콘 상태 변경 (열림/닫힘에 따라)
         if (companyInfo.classList.contains('footer__company-info--active')) {
-            icon.src = '/src/assets/icons/icon-chevron-up.png'; // up 아이콘
+            footerIcon.classList.replace('_chevron_thick_down_12', '_chevron_thick_up_12');
         } else {
-            icon.src = '/src/assets/icons/icon-chevron-down.png'; // down 아이콘
+            footerIcon.classList.replace('_chevron_thick_up_12', '_chevron_thick_down_12');
         }
     });
 });
