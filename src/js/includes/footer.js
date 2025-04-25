27dba5a9a5995bda06716a25d52a40c3ce8d0f52
로인 <151013952+bboroin@users.noreply.github.com>
2025-04-25 16:37:35 +0900

    style: --mobile과 --web 클래스명 구분 네이밍 #33

--- src/js/includes/footer.js
+++ src/js/includes/footer.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('footer__company-btn').addEventListener('click', function () {
         const footerIcon = document.querySelector('.footer__company-icon');
-        const companyInfo = document.querySelector('.footer__company-info');
+        const companyMobile = document.querySelector('.footer__company-details-mobile');
 
         // footer__company-info의 클래스 토글 (열리고 닫히는 동작)
-        companyInfo.classList.toggle('footer__company-info--active');
+        companyMobile.classList.toggle('footer__company-details-mobile--active');
 
         // 아이콘 상태 변경 (열림/닫힘에 따라)
-        if (companyInfo.classList.contains('footer__company-info--active')) {
+        if (companyMobile.classList.contains('footer__company-details-mobile--active')) {
             footerIcon.classList.replace('_chevron_thick_down_12', '_chevron_thick_up_12');
         } else {
             footerIcon.classList.replace('_chevron_thick_up_12', '_chevron_thick_down_12');
